36c1739e7c03547b7b32c19009efbf2f05d154e9
luhouyang <luhouyang@gmail.com>
2024-08-03 14:59:48 +0800

    testing server script updated

--- model/temp/request.dart
+++ model/temp/request.dart
@@ -1,28 +1,58 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {
-  classifyData("Ka-ching! Incoming money | aelbgla rg gg hehe boi Transaction to Hans, RM 10 with Touch n go");
+void main() async {
+  int res1 = await classifyData(
+                          "Algorithm & Data Structure: Shashi Baka (OOP Lecturer) | Graphs.pptx");
+  int res2 = await classifyData(
+      "Ka-ching! Incoming money | Transaction to Hans, RM 10 with Touch n go");
+  int res3 = await classifyData(
+      "DuitNow Payment | You have paid RM6.00 to island one cafe and bakery.");
 }
 
-Future<List<String>> classifyData(String message) async {
-  List<String> resList = [];
+Future<int> classifyData(String message) async {
+    Map<String, int> responseClasses = {
+      "NOT TRANSACTION": 0,
+      "MONEY OUT": 1,
+      "MONEY IN": 2
+    };
 
-  final url = Uri.parse('http://47.250.87.162:9000/api/classify');
-  final response = await http.post(
-    url,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: json.encode({'data': message}),
-  );
+    List<String> resList = [];
 
-  if (response.statusCode == 200) {
-    final data = json.decode(response.body);
-    print('Prediction: ${data['prediction']}');
-  } else {
-    print('Failed to load data');
+    final url = Uri.parse('http://47.250.87.162:9000/api/classify');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({'data': message}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      resList.add(data['prediction']);
+      // print('Prediction: ${data['prediction']}');
+    } else {
+      resList.add("ERROR");
+      // print('Failed to load data');
+    }
+
+    if (resList[0] == "ERROR") {
+      print("Something went wong");
+      return -1;
+    } else {
+      if (responseClasses[resList[0]] == 0) {
+        print("NOT A TRANSACTION");
+        return 0;
+      } else if (responseClasses[resList[0]] == 1) {
+        print("MONEY OUT");
+        return 1;
+      } else if (responseClasses[resList[0]] == 2) {
+        print("MONEY IN");
+        return 2;
+      }
+    }
+
+    print("Something went wong");
+    return -1;
   }
-
-  return resList;
-}
