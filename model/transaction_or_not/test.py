9ddc43ce00aebd8c60cec8fef9a56f038f831524
luhouyang <luhouyang@gmail.com>
2024-08-03 11:54:20 +0800

    SVC model and vectorizer

---
+++ model/transaction_or_not/test.py
@@ -0,0 +1,63 @@
+#%%
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+import pandas as pd
+import joblib
+import pickle
+
+### remove
+rawdata = pd.read_csv('testdata.csv')
+rawdf = pd.DataFrame(rawdata)
+
+text = rawdf.iloc[:, 0]
+labels = rawdf.iloc[:, 1]
+###
+
+# load vectorizer and vector support machine
+transaction_or_not_feature_map = {
+    0: "NOT TRANSACTION",
+    1: "TRANSACTION"
+}
+
+clf = joblib.load('transaction_or_not.pkl')
+
+with open('transaction_or_not_vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+
+### remove
+vector_words = vectorizer.transform(text)
+###
+
+###
+from sklearn.metrics import accuracy_score, classification_report
+
+# Predict on the test set
+y_pred = clf.predict(vector_words)
+
+# Evaluate the performance
+accuracy = accuracy_score(y_pred, labels)
+report = classification_report(y_pred, labels, target_names=[transaction_or_not_feature_map[0], transaction_or_not_feature_map[1]])
+
+print(f'Accuracy: {accuracy:.4f}')
+print('Classification Report:')
+print(report)
+### remove
+
+
+# predict one class
+def predict_category(text):
+    """
+    Predict the category of a given text using the trained classifier.
+    """
+    text_vec = vectorizer.transform([text])
+    prediction = clf.predict(text_vec)
+    return transaction_or_not_feature_map[prediction[0]]
+
+# Example usecase
+sample_text = "Transaction to Ali RM 2"
+predicted_category = predict_category(sample_text)
+print(f'The predicted category is: {predicted_category}')
+#%%
---
+++ model/transaction_or_not/transaction_normal_message_clf.py
@@ -0,0 +1,72 @@
+#%%
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+from IPython.display import display
+
+import pandas as pd
+
+
+transaction_or_not_feature_map = {
+    0: "NOT TRANSACTION",
+    1: "TRANSACTION"
+}
+
+
+rawdata = pd.read_csv('testdata.csv')
+rawdf = pd.DataFrame(rawdata)
+
+text = rawdf.iloc[:, 0]
+labels = rawdf.iloc[:, 1]
+
+display(rawdf.head())
+
+vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+vector_words = vectorizer.fit_transform(text)
+
+print(vectorizer.get_feature_names_out())
+
+# X_train, X_test, y_train, y_test = train_test_split(vector_words, labels)
+
+clf = SVC(kernel='linear')
+clf.fit(vector_words, labels)
+
+
+from sklearn.metrics import accuracy_score, classification_report
+
+# Predict on the test set
+y_pred = clf.predict(vector_words)
+
+# Evaluate the performance
+accuracy = accuracy_score(y_pred, labels)
+report = classification_report(y_pred, labels, target_names=[transaction_or_not_feature_map[0], transaction_or_not_feature_map[1]])
+
+print(f'Accuracy: {accuracy:.4f}')
+print('Classification Report:')
+print(report)
+
+
+def predict_category(text):
+    """
+    Predict the category of a given text using the trained classifier.
+    """
+    text_vec = vectorizer.transform([text])
+    prediction = clf.predict(text_vec)
+    return transaction_or_not_feature_map[prediction[0]]
+
+# Example usage
+sample_text = "Not NASA announced the discovery of new exoplanets. There are chickens on there"
+predicted_category = predict_category(sample_text)
+print(f'The predicted category is: {predicted_category}')
+
+
+import joblib
+import pickle
+
+joblib.dump(clf, 'transaction_or_not.pkl')
+
+with open('transaction_or_not_vectorizer.pkl', 'wb') as f:
+    pickle.dump(vectorizer, f)
+
+#%%
